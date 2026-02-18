2be1d40b5410f00b46d3f8cc4842a86d31c480de
gabrieltomazi <tomazigabriel3@gmail.com>
2026-02-17 21:47:14 -0300

    config: update API base URL to production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://cadastro-de-usuario-q2ny.onrender.com'
 })
 
 export default api
